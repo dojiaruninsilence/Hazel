b453a98fecfd77431404fc64e699c491ac60400f
Zubin Falberg <the.z.creations@gmail.com>
2024-02-17 12:30:32 -0700

    Added OpenGL context class also aswitched to static library in last commit and was not mentioned

--- Hazel/src/Platform/Windows/WindowsWindow.h
+++ Hazel/src/Platform/Windows/WindowsWindow.h
@@ -3,6 +3,7 @@
 #include "hzpch.h"
 
 #include "Hazel/Window.h"
+#include "Hazel/Renderer/GraphicsContext.h"
 
 #include <GLFW/glfw3.h>
 
@@ -30,6 +31,7 @@ namespace Hazel {
 		virtual void Shutdown();
 	private:
 		GLFWwindow* m_Window;
+		GraphicsContext* m_Context;
 
 		struct WindowData
 		{
