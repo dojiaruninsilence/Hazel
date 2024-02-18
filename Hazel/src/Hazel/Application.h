5618756e5415c9d885a04e307830133731f76444
Zubin Falberg <the.z.creations@gmail.com>
2024-02-18 08:22:25 -0700

    first triangle rendered on the screen and the first shader

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -11,6 +11,8 @@
 
 #include "Hazel/ImGui/ImGuiLayer.h"
 
+#include "Hazel/Renderer/Shader.h"
+
 namespace Hazel {
 
 	class HAZEL_API Application
@@ -36,6 +38,9 @@ namespace Hazel {
 		ImGuiLayer* m_ImGuiLayer;
 		bool m_Running = true;
 		LayerStack m_LayerStack;
+
+		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		std::unique_ptr<Shader> m_Shader;
 	private:
 		static Application* s_Instance;
 	};
